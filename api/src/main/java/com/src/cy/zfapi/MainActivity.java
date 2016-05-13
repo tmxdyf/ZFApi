7e3f2786a366cd13d440e4790f1ca95ab8dcdbce
cy <tmxdyf@163.com>
2016-05-13 09:33:06 +0800

    commit by cy

--- api/src/main/java/com/src/cy/zfapi/MainActivity.java
+++ api/src/main/java/com/src/cy/zfapi/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
