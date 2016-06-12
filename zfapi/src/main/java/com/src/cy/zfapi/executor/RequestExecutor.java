391df68574effec2b293ba82ad2621b03abaab9d
tmxdyf <tmxdyf@163.com>
2016-06-12 16:33:47 +0800

    commit by cy

--- zfapi/src/main/java/com/src/cy/zfapi/executor/RequestExecutor.java
+++ zfapi/src/main/java/com/src/cy/zfapi/executor/RequestExecutor.java
@@ -8,8 +8,10 @@ import com.src.cy.zfapi.api.ApiService;
 import com.src.cy.zfapi.factory.StringConverterFactory;
 import com.src.cy.zfapi.util.DESUtil;
 
+import java.io.IOException;
 import java.util.Map;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
@@ -17,6 +19,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -29,16 +32,19 @@ public class RequestExecutor {
 
     static {
 
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(chain -> {
-            Response response = chain.proceed(chain.request());
-            ResponseBody responseBody = response.body();
-            String result = responseBody.string();
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Response response = chain.proceed(chain.request());
+                ResponseBody responseBody = response.body();
+                String result = responseBody.string();
 
-            result = ZFApi.isEncrypt() ? DESUtil.decryptDoNet(result) : result;//解密
-            Log.e("Client", "intercept#result:" + result);
-            ResponseBody newResponseBody = ResponseBody.create(responseBody.contentType(), result);
-            Response newResponse = response.newBuilder().body(newResponseBody).build();
-            return newResponse;
+                result = ZFApi.isEncrypt() ? DESUtil.decryptDoNet(result) : result;//解密
+                Log.e("Client", "intercept#result:" + result);
+                ResponseBody newResponseBody = ResponseBody.create(responseBody.contentType(), result);
+                Response newResponse = response.newBuilder().body(newResponseBody).build();
+                return newResponse;
+            }
         }).build();
 
 //        初始化Retrofit
@@ -57,14 +63,19 @@ public class RequestExecutor {
      * 请求网络入口
      *
      * @param method 请求Web的方法体
-     * @param map 需要上传的参数与内容
-     * @param cla 返回的结果内容反射的类
-     * @param <T> 数据实体类
+     * @param map    需要上传的参数与内容
+     * @param cla    返回的结果内容反射的类
+     * @param <T>    数据实体类
      * @return 返回观察者对象
      */
-    public static <T> Observable<T> request(String method, Map<String, Object> map, Class<T> cla) {
+    public static <T> Observable<T> request(String method, Map<String, Object> map, final Class<T> cla) {
         Observable<String> observable = getApiService().post2(method, ZFApi.isEncrypt() ? encryptMap(map) : map).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread());
-        return observable.map(s -> new Gson().fromJson(s, cla));
+        return observable.map(new Func1<String, T>() {
+            @Override
+            public T call(String s) {
+                return new Gson().fromJson(s, cla);
+            }
+        });
     }
 
     /**
